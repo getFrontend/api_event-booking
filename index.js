e0c2cabf2bc04dc24ac6ee44a841ca33c22e6ca7
Sergey UP <sergey10goals@gmail.com>
2023-11-21 12:34:34 +0200

    import node:fs & create server

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const http = require('node:http');
+import http from 'node:http';
+import fs from "node:fs/promises";
+
+const PORT = 8080;
+
+http
+  .createServer(async (req, res) => {
+    if (req.method === 'GET' && req.url === '/artists') {
+      try {
+        const data = await fs.readFile('data/comedians.json', 'utf-8', (err, data) => {
+        });
+        res.writeHead(200, {
+          "Content-Type": "text/json; charset=utf-8",
+          "Access-Control-Allow-Origin": "*",
+        });
+        res.end(data);
+      } catch (error) {
+        res.writeHead(500, {
+          "Content-Type": "text/plain; charset=utf-8",
+        });
+        res.end(`Server Error: ${error}`);
+      }
+    } else {
+      res.writeHead(404, {
+        "Content-Type": "text/json; charset=utf-8",
+      });
+      res.end('404: page is not found')
+    }
+  })
+  .listen(PORT);
+
+  console.log(`Server is running on http://localhost:${PORT}`)
