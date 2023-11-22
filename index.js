351224aeceac37e5c5f583337d6b2a0a21cb924e
Sergey UP <sergey10goals@gmail.com>
2023-11-22 15:49:28 +0200

    add checkFile function

--- index.js
+++ index.js
@@ -2,14 +2,40 @@ import http from 'node:http';
 import fs from "node:fs/promises";
 
 const PORT = 8080;
+const COMEDIANS = 'data/comedians.json';
+const CLIENTS = 'data/clients.json';
 
-const startServer = async () => {
+const checkFiles = async () => {
+  try {
+    await fs.access(COMEDIANS);
+  } catch (error) {
+    console.error(`File ${COMEDIANS} is not found`)
+    return false;
+  }
+
+  try {
+    await fs.access(CLIENTS);
+    return true;
+  } catch (error) {
+    await fs.writeFile(CLIENTS, JSON.stringify([]));
+    console.log(`File ${CLIENTS} was sucssessfully created`)
+    return false;
+  }
+}
+
+const startServer = async (req, res) => {
+  if (!(await checkFiles())) {
+    return;
+  }
+  
   http
-    .createServer(async (req, res) => {
+  .createServer(async (req, res) => {
+      const reqURL = new URL(req.url, `http://${req.headers.host}`);
+      const pathName = req.utl.pathName;
+
       if (req.method === 'GET' && req.url === '/artists') {
         try {
-          const data = await fs.readFile('data/comedians.json', 'utf-8', (err, data) => {
-          });
+          const data = await fs.readFile(COMEDIANS, 'utf-8');
           res.writeHead(200, {
             "Content-Type": "text/json; charset=utf-8",
             "Access-Control-Allow-Origin": "*",
