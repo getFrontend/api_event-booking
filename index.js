2549f206a1a0bebc7497403d4bec111c5a9f0b4e
Sergey UP <sergey10goals@gmail.com>
2023-11-22 14:03:14 +0200

    add startServer function

--- index.js
+++ index.js
@@ -3,30 +3,34 @@ import fs from "node:fs/promises";
 
 const PORT = 8080;
 
-http
-  .createServer(async (req, res) => {
-    if (req.method === 'GET' && req.url === '/artists') {
-      try {
-        const data = await fs.readFile('data/comedians.json', 'utf-8', (err, data) => {
-        });
-        res.writeHead(200, {
+const startServer = async () => {
+  http
+    .createServer(async (req, res) => {
+      if (req.method === 'GET' && req.url === '/artists') {
+        try {
+          const data = await fs.readFile('data/comedians.json', 'utf-8', (err, data) => {
+          });
+          res.writeHead(200, {
+            "Content-Type": "text/json; charset=utf-8",
+            "Access-Control-Allow-Origin": "*",
+          });
+          res.end(data);
+        } catch (error) {
+          res.writeHead(500, {
+            "Content-Type": "text/plain; charset=utf-8",
+          });
+          res.end(`Server Error: ${error}`);
+        }
+      } else {
+        res.writeHead(404, {
           "Content-Type": "text/json; charset=utf-8",
-          "Access-Control-Allow-Origin": "*",
         });
-        res.end(data);
-      } catch (error) {
-        res.writeHead(500, {
-          "Content-Type": "text/plain; charset=utf-8",
-        });
-        res.end(`Server Error: ${error}`);
+        res.end('404: page is not found')
       }
-    } else {
-      res.writeHead(404, {
-        "Content-Type": "text/json; charset=utf-8",
-      });
-      res.end('404: page is not found')
-    }
-  })
-  .listen(PORT);
+    })
+    .listen(PORT);
 
-  console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(`Server is running on http://localhost:${PORT}`);
+}
+
+startServer();
