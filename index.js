02d87cf5e757781e3f40f7ce9430de840ae23d8d
Sergey UP <sergey10goals@gmail.com>
2023-11-23 21:21:13 +0200

    fix module import error

--- index.js
+++ index.js
@@ -1,7 +1,87 @@
-import { startServer } from './modules/startServer';
+import http from 'node:http';
+import fs from "node:fs/promises";
+import { sendData, sendError } from './modules/send.js';
 
-const init = () => {
-  startServer();
+const COMEDIANS = 'data/comedians.json';
+const CLIENTS = 'data/clients.json';
+const PORT = 8080;
+
+const checkFiles = async () => {
+  try {
+    await fs.access(COMEDIANS);
+  } catch (error) {
+    console.error(`File ${COMEDIANS} is not found`)
+    return false;
+  }
+
+  try {
+    await fs.access(CLIENTS);
+  } catch (error) {
+    await fs.writeFile(CLIENTS, JSON.stringify([]));
+    console.log(`File ${CLIENTS} was sucssessfully created`)
+    return false;
+  }
+
+  return true;
 }
 
-init();
+const startServer = async () => {
+  if (!(await checkFiles())) {
+    return;
+  }
+
+  http
+    .createServer(async (req, res) => {
+      try {
+        const segments = req.url.split('/').filter(Boolean);
+
+        if (req.method === 'GET' && segments[0] === 'artists') {
+          const comediansData = await fs.readFile(COMEDIANS, 'utf-8');
+          const comedians = JSON.parse(comediansData);
+
+          if (segments.length === 2) {
+            const comedian = comedians.find((c) => c.id === segments[1]);
+
+            if (!comedian) {
+              sendError(res, 404, 'Sorry, but this comedian is not found!')
+              return;
+            }
+
+            sendData(res, comedians);
+            return;
+          }
+          sendData(res, data);
+          return;
+        }
+
+        if (req.method === 'POST' && segments[0] === 'clients') {
+          console.log('Post: add client')
+        }
+
+        if (
+          req.method === 'GET' &&
+          segments[0] === 'clients' &&
+          segments.length === 2
+        ) {
+          console.log('Get: get client by ticket number')
+        }
+
+        if (
+          req.method === 'PATCH' &&
+          segments[0] === 'clients' &&
+          segments.length === 2
+        ) {
+          console.log('Patch: update client by ticket number')
+        }
+
+        sendError(res, 404, '404: page is not found');
+      } catch (error) {
+        sendError(res, 500, `Server Error: ${error}`)
+      }
+    })
+    .listen(PORT);
+
+  console.log(`Server is running on http://localhost:${PORT}`);
+}
+
+startServer();
--- modules/sendData.js
+++
@@ -1,6 +0,0 @@
-export const sendData = (res, data) => {
-  res.writeHead(200, {
-    "Content-Type": "text/json; charset=utf-8",
-  });
-  res.end(data);
-}
